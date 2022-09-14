9a5e8e32506d975cb1df94765e0d081dd46a386e
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-09-14 23:23:01 +0100

    Many Changes fak

--- Source/King/Datas/Assets/King_SkillItem.cpp
+++ Source/King/Datas/Assets/King_SkillItem.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Datas/Assets/MyKing_SkillItem.h"
+#include "Datas/Assets/King_SkillItem.h"
 
--- Source/King/Datas/Assets/King_SkillItem.h
+++ Source/King/Datas/Assets/King_SkillItem.h
@@ -4,14 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Datas/King_Item.h"
-#include "MyKing_SkillItem.generated.h"
+#include "King_SkillItem.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class KING_API UMyKing_SkillItem : public UKing_Item
+class KING_API UKing_SkillItem : public UKing_Item
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	FName SkillName;
 };
