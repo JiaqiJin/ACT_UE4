1f7d1b657962e26d6cf823f9ff7ad490bc7e5d37
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-09-14 19:58:27 +0100

    update

---
+++ Source/King/Datas/Assets/King_SkillItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Datas/King_Item.h"
+#include "MyKing_SkillItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KING_API UMyKing_SkillItem : public UKing_Item
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/King/Datas/King_AssetManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Datas/King_AssetManager.h"
+
+const FPrimaryAssetType UKing_AssetManager::SkillType = TEXT("SkillType");
+const FPrimaryAssetType UKing_AssetManager::MotionType = TEXT("MotionType");
+
+UKing_AssetManager::UKing_AssetManager()
+{
+
+}
+
+UKing_AssetManager& UKing_AssetManager::Get()
+{
+	UKing_AssetManager* Singleton = Cast<UKing_AssetManager>(GEngine->AssetManager);
+
+	if (Singleton)
+	{
+		return *Singleton;
+	}
+	else
+	{
+		//UE_LOG(LogAssetManager, Fatal, TEXT("Cannot use AssetManager if no AssetManagerClassName is defined!"));
+		return *NewObject<UKing_AssetManager>(); // never calls this
+	}
+}
